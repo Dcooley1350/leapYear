7d6788ddbee364d54c3601afaee7c6984a078986
Devin Cooley <dcooley1350@gmail.com>
2019-06-19 15:47:46 -0700

    add spec for div. by 400 and by 100.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,12 @@
 var leapYear = function(year) {
-  return false;
-};
-var leapYear = function(year) {
-  if (year % 4 === 0) {
+  if ((year % 4 === 0) && (year % 100 !== 0)) {
     return true;
+  } else if (year % 400 === 0) {
+    return true
   } else {
     return false;
-  };
+  }
 };
-
 
 
 $(document).ready(function() {
