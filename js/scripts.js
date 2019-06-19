a9a87b2d5d2f41e88eaf4cd42b9a4348c230b85d
Devin Cooley <dcooley1350@gmail.com>
2019-06-19 15:20:13 -0700

    add spec to return false if year does not = leap year.

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,7 @@ var leapYear = function(year) {
 $(document).ready(function() {
   $("form#leap-year").submit(function(event) {
     event.preventDefault();
-    var year = parsInt($("input#year").val());
+    var year = parseInt($("input#year").val());
     var result = leapYear(year);
     $("#result").text(result);
   });
